32d5b70cd503ece1b8d53fd4df6829ebe235c687
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-24 13:21:32 +0200

    Advanced query builder usage with clearSelect operation on the query builder

--- src/operations/queryBuilder.ts
+++ src/operations/queryBuilder.ts
@@ -4,25 +4,25 @@ import { Author } from "../types"
 export const getAuthorsPaginated = async (
   limit: number,
   offset: number
-): Promise<{ results: Author[]; count: number }> => {
+): Promise<{
+  results: Partial<Author>[]
+  count: number
+}> => {
   const queryBuilder = knex("authors")
-    // .select("id") // this would break the cloned query builder as it is now
+    .select("id")
     .where("name", "like", "%a%")
-  // First, use the query builder to await the authors themselves
+
   const authors = await queryBuilder
     .limit(limit)
     .offset(offset)
 
-  // Then, clone it to count the number of records that the original
-  // query builder would return
-  // If we insisted on selecting only the 'id' column in the original query builder,
-  // then we would need to clear selects in this query
   const count = Number(
     (
       await queryBuilder
         .clone()
+        .clearSelect()
         .count()
-        ./* clearSelect(). */ first()
+        .first()
     )?.count
   )
 
