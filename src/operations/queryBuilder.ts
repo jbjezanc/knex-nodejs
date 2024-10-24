a84c0d2ca1c3a63a42670d115ae66d054154cbbb
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-24 13:02:24 +0200

    Create a query builder - a normal query that is not awaited so that we can build statements and commands on top of that object; we can clone a query builder and remove some of the statements like clearSelect, clearCounters, clearGroup; add new where conditions etc.

--- src/operations/queryBuilder.ts
+++ src/operations/queryBuilder.ts
@@ -7,9 +7,26 @@ export const getAuthorsPaginated = async (
   limit: number,
   offset: number
 ): Promise<{ results: Author[]; count: number }> => {
-  const authors = await knex("authors")
-    .limit(limit)
-    .offset(offset)
+  // the type of this constant is a Knex.QueryBuilder<..,..>
+  const queryBuilder = knex("authors")
+  // so we can build on top of it
+  queryBuilder.limit(limit)
+  queryBuilder.offset(offset)
+
+  // cloning the queryBuilder will not affect the original query
+  const queryBuilderTwo = queryBuilder.clone()
+  queryBuilderTwo.where('id', 10)
+  // we can clear some statements added by the original queryBuilder
+  queryBuilderTwo.clearSelect()
+  // we can clear count statements
+  queryBuilderTwo.clearCounters()
+  // we can clear groupBy statements
+  queryBuilderTwo.clearGroup()
+
+  // finally, we can execute the queryBuilderTwo
+  const result = await queryBuilderTwo
+
+  const authors = await queryBuilder
   const count = Number(
     (await knex("authors").count().first())?.count
   )
